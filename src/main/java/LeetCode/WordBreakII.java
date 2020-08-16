853041ad4c4c58b4642c2a3f045bbff638e86a93
ayush1312 <ayush.abhishek13@gmail.com>
2020-08-16 12:20:01 +0530

    thetechgranth commit

---
+++ src/main/java/LeetCode/WordBreakII.java
@@ -0,0 +1,35 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WordBreakII {
+    Map<String, List<String>> map = new HashMap<String,List<String>>();
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        List<String> res = new ArrayList<String>();
+        if(s == null || s.length() == 0) {
+            return res;
+        }
+        if(map.containsKey(s)) {
+            return map.get(s);
+        }
+        if(wordDict.contains(s)) {
+            res.add(s);
+        }
+        for(int i = 1 ; i < s.length() ; i++) {
+            String t = s.substring(i);
+            if(wordDict.contains(t)) {
+                List<String> temp = wordBreak(s.substring(0 , i) , wordDict);
+                if(temp.size() != 0) {
+                    for(int j = 0 ; j < temp.size() ; j++) {
+                        res.add(temp.get(j) + " " + t);
+                    }
+                }
+            }
+        }
+        map.put(s , res);
+        return res;
+    }
+}
