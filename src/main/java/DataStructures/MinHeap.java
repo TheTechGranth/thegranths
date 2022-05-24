f151badc966bd31929377d52eea53c8d31aac684
ayush1312 <ayush.abhishek13@gmail.com>
2022-05-25 00:54:41 +0530

    thetechgranth commit

---
+++ src/main/java/DataStructures/MinHeap.java
@@ -0,0 +1,105 @@
+package DataStructures;
+
+import java.util.Arrays;
+
+public class MinHeap {
+    int capacity;
+    int size = 0;
+    int[] heap;
+
+    public MinHeap(int capacity) {
+        this.capacity = capacity;
+        heap = new int[capacity];
+    }
+
+    private int getLeftChildIndex (int parentIndex) { return (2*parentIndex) + 1; }
+    private int getRightChildIndex (int parentIndex) { return (2*parentIndex) + 2; }
+    private int getParentIndex(int childIndex) { return (childIndex-1)/2;}
+
+    //private int getLeftChild (int parentIndex) { return heap[getLeftChildIndex(parentIndex)]; }
+    //private int getRightChild (int parentIndex) { return heap[getRightChildIndex(parentIndex)]; }
+    private int getParent(int childIndex) { return heap[getParentIndex(childIndex)]; }
+
+    // here reSizeHeap method can also take a load factor as input
+    private void reSizeHeap() {
+        if(size == capacity) {
+            heap = Arrays.copyOf(heap, capacity * 2);
+            capacity = capacity * 2;
+        }
+    }
+
+    public int peek(){
+        if (size == 0) throw new IllegalStateException("Empty Heap");
+        return heap[0];
+    }
+
+    public void insert (int val){
+        reSizeHeap();
+        heap[size] = val;
+        size++;
+        heapifyUp();
+    }
+
+    public int poll(){
+        if (size == 0) throw new IllegalStateException("Empty Heap");
+        int val = heap[0];
+        heap[0] = heap[size-1];
+        size--;
+        heapifyDown();
+        return val;
+    }
+
+    private void heapifyDown() {
+       int index = 0;
+       while(getLeftChildIndex(index) < size){
+           int smallerIndex = index;
+           if(heap[getLeftChildIndex(index)] < heap[smallerIndex]) smallerIndex = getLeftChildIndex(index);
+           if(heap[getRightChildIndex(index)] < heap[smallerIndex] ) smallerIndex = getRightChildIndex(index);
+           if(smallerIndex != index) {
+               swap(smallerIndex, index);
+               index = smallerIndex;
+           }else break;
+
+       }
+    }
+
+    private void heapifyUp() {
+        int index = size - 1;
+        while(getParentIndex(index) >=0){
+            if(getParent(index) > heap[index]) {
+                swap(getParentIndex(index), index);
+                index = getParentIndex(index);
+            }else break;
+        }
+    }
+
+    private void swap(int parentIndex, int index) {
+        int val = heap[parentIndex];
+        heap[parentIndex] = heap[index];
+        heap[index] = val;
+    }
+
+    public void printHeap(){
+        for (int i =0;i<size;i++) {
+            System.out.print(heap[i] + "->");
+        }
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+        MinHeap minHeap = new MinHeap(5);
+        minHeap.insert(4);
+        minHeap.insert(10);
+        minHeap.insert(3);
+        minHeap.insert(5);
+        minHeap.insert(1);
+        minHeap.printHeap();
+        minHeap.peek();
+        minHeap.printHeap();
+        minHeap.poll();
+        minHeap.printHeap();
+        minHeap.insert(1);
+        minHeap.printHeap();
+    }
+
+}
